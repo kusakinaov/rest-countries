7fe65839efe8a8be377aff5066d8bd9b4c5f7cef
kusakinaov <kusakinaov@gmail.com>
2020-02-28 08:35:10 +0300

    bind country fields and hides empty views

--- app/src/main/java/olga/ku/restcountries/fragment/CountryFragment.kt
+++ app/src/main/java/olga/ku/restcountries/fragment/CountryFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_country.view.*
+import kotlinx.android.synthetic.main.fragment_country.view.textViewName
 import olga.ku.restcountries.R
 import olga.ku.restcountries.model.Country
 import olga.ku.restcountries.model.Currency
@@ -34,34 +36,65 @@ class CountryFragment : Fragment() {
     private fun setCountry(country: Country) {
         activity?.title = country.name
         view?.apply {
-            textViewName.text = country.name
-            textViewDomain.text = country.topLevelDomain?.joinToString(separator = ", ")
-            textViewAlpha2Code.text = country.alpha2Code
-            textViewAlpha3Code.text = country.alpha3Code
-            textViewCallingCodes.text = country.callingCodes?.joinToString(separator = ", ")
-            textViewCapital.text = country.capital
-            textViewAltSpellings.text = country.altSpellings?.joinToString(separator = ", ")
-            textViewRegion.text = country.region
-            textViewSubRegion.text = country.subregion
-            textViewPopulation.text = country.population?.toString()
-            textViewLatLon.text = country.latlng?.joinToString(", ") { d -> d.toString() }
-            textViewDemonym.text = country.demonym
-            textViewArea.text = country.area?.toString()
-            textViewGini.text = country.gini?.toString()
-            textViewTimezones.text = country.timezones?.joinToString(separator = ", ")
-            textViewBorders.text = country.borders?.joinToString(separator = ", ")
-            textViewNativeName.text = country.nativeName
-            textViewNumericCode.text = country.numericCode
-            textViewCurrencies.text =
-                country.currencies?.joinToString(separator = "\n") { currency -> currency.fullLine() }
-            textViewLanguages.text =
-                country.languages?.joinToString(separator = "\n") { language -> language.fullLine() }
-            //translations
-            textViewFlag.text = country.flag
-            textViewRegionalBlocs.text =
-                country.regionalBlocs?.joinToString(separator = "\n") { regionalBlock -> regionalBlock.fullLine() }
-            textViewCioc.text = country.cioc
+            setValue(textViewHeaderName, textViewName, country.name)
+            setValue(
+                textViewHeaderDomain,
+                textViewDomain,
+                country.topLevelDomain?.joinToString(separator = ", ")
+            )
+            setValue(textViewHeaderAlpha2Code, textViewAlpha2Code, country.alpha2Code)
+            setValue(textViewHeaderAlpha3Code, textViewAlpha3Code, country.alpha3Code)
+            setValue(
+                textViewHeaderCallingCodes,
+                textViewCallingCodes,
+                country.callingCodes?.joinToString(separator = ", ")
+            )
+            setValue(textViewHeaderCapital, textViewCapital, country.capital)
+            setValue(
+                textViewHeaderAltSpellings,
+                textViewAltSpellings,
+                country.altSpellings?.joinToString(separator = ", ")
+            )
+            setValue(textViewHeaderRegion, textViewRegion, country.region)
+            setValue(textViewHeaderSubRegion, textViewSubRegion, country.subregion)
+            setValue(textViewHeaderPopulation, textViewPopulation, country.population?.toString())
+            setValue(
+                textViewHeaderLatLon,
+                textViewLatLon,
+                country.latlng?.joinToString(", ") { it.toString() })
+            setValue(textViewHeaderDemonym, textViewDemonym, country.demonym)
+            setValue(textViewHeaderArea, textViewArea, country.area?.toString())
+            setValue(textViewHeaderGini, textViewGini, country.gini?.toString())
+            setValue(
+                textViewHeaderTimezones,
+                textViewTimezones,
+                country.timezones?.joinToString(separator = ", ")
+            )
+            setValue(
+                textViewHeaderBorders,
+                textViewBorders,
+                country.borders?.joinToString(separator = ", ")
+            )
+            setValue(textViewHeaderNativeName, textViewNativeName, country.nativeName)
+            setValue(textViewHeaderNumericCode, textViewNumericCode, country.numericCode)
+            setValue(textViewHeaderCurrencies, textViewCurrencies,
+                country.currencies?.joinToString(separator = "\n") { it.fullLine() })
+            setValue(textViewHeaderLanguages, textViewLanguages,
+                country.languages?.joinToString(separator = "\n") { it.fullLine() })
+            setValue(textViewHeaderTranslations, textViewTranslations, null)
+            setValue(textViewHeaderFlag, textViewFlag, country.flag)
+            setValue(textViewHeaderRegionalBlocs, textViewRegionalBlocs,
+                country.regionalBlocs?.joinToString(separator = "\n") { it.fullLine() })
+            setValue(textViewHeaderCioc, textViewCioc, country.cioc)
         }
+    }
+
+    private fun setValue(textViewHeader: TextView, textView: TextView, text: String?) {
+        textView.text = text
+
+        val visibility = if (text.isNullOrEmpty()) View.GONE else View.VISIBLE
+        textViewHeader.visibility = visibility
+        textView.visibility = visibility
     }
 
     private fun Currency.fullLine() = getString(R.string.pattern_currency, name, code, symbol)
